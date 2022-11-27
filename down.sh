a9fadaa9c78c6f3d814cd1dfbf7a6a60635bf3db
Tomas Brejla <brdloush@gmail.com>
2022-11-27 10:37:19 +0100

    fix path to bb

--- down.sh
+++ down.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #wget --remote-encoding=cp1250 -r -N --cut-dirs=2 -nH --accept="zip,ZIP" --reject-regex='2021-12/*|2022-01/*' ftp://ftp.cisjr.cz/draha/celostatni/szdc/2022
 TARGET_DIR=`pwd`/szdc/2022
-bb dl_http_folders.clj https://portal.cisjr.cz /pub/draha/celostatni/szdc/2022 $TARGET_DIR
+./bb dl_http_folders.clj https://portal.cisjr.cz /pub/draha/celostatni/szdc/2022 $TARGET_DIR
