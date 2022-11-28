98cff6203369a719d9a890bd83d509d2cafb64e4
Tomas Brejla <brdloush@gmail.com>
2022-11-28 23:40:35 +0100

    use download_dirlisting.clj script from github instead of local one

--- download_dirlisting_downloader.sh
+++ download_dirlisting_downloader.sh
@@ -3,3 +3,4 @@ wget https://github.com/babashka/babashka/releases/download/v1.0.166/babashka-1.
 tar xvvzf babashka-1.0.166-linux-amd64-static.tar.gz
 chmod +x ./bb
 rm babashka-1.0.166-linux-amd64-static.tar.gz
+wget https://raw.githubusercontent.com/brdloush/download-dirlisting/master/download_dirlisting.clj
